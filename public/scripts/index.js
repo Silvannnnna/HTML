b1fa58ef1494840784c90318cebcad487c3cf20e
Silvanna Farias <silvanna@Silvannas-MacBook-Pro-2.local>
2025-03-03 16:54:09 -0600

    “api”

---
+++ public/scripts/index.js
@@ -0,0 +1,26 @@
+console.log("hola desde java");
+console.log("Holi")
+
+//const username = document.getElementById("username");
+//const password = document.getElementById("password");
+
+//const login = () => {
+  // console.log(username.value, password.value);
+  //if (username.value === "Silvanna" && password.value === "123") {
+    //sessionStorage.setItem("name", "Silvanna Farías");
+    //window.location = "/pages/home.html";
+  //} else {
+    //alert("credenciales incorrectas");
+  //}
+//};
+
+//const boton = document.getElementById("boton");
+
+//boton.addEventListener("click", login);
+
+//password.addEventListener("keydown", (e) => {
+  //if (e.key === "Enter") {
+    //login();
+  //}
+//});
+
